bc1f76e56fd66f629234474aeac450386413869c
Admin <Admin@DESKTOP-3Q3UHU7>
2025-07-28 03:21:41 +0500

    Add project files.

---
+++ MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Skills()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
